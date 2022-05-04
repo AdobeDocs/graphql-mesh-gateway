df95eef160f43f1cefb2b635288925b98885041a
Kevin Harper <keharper@users.noreply.github.com>
2022-05-04 09:30:18 -0500

    Update src/data/navigation/sections/guides.js
    Co-authored-by: Jared Hoover <98363870+jhadobe@users.noreply.github.com>

--- src/data/navigation/sections/guides.js
+++ src/data/navigation/sections/guides.js
@@ -10,7 +10,7 @@ module.exports = [
         },
         {
             title: 'Adobe Graph',
-            path: '/guides/gateway/index/'
+            path: '/guides/gateway/'
         },
     ],
 },
