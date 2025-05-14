24ee6ffb7ecbd5a3364f2ebdc9cfa73965dfa2ab
Dima Shevtsov <shevtsov@adobe.com>
2025-05-14 14:40:20 -0500

    Add copyright header, delete duplicate, fix EOF

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,10 @@
+/*
+ *  Copyright 2025 Adobe All Rights Reserved.
+ *  NOTICE:  All information contained herein is, and remains the property of Adobe and its suppliers, if any.
+ *  The intellectual and technical concepts contained herein are proprietary to Adobe and its suppliers and are protected by all applicable intellectual property laws, including trade secret and copyright laws.
+ *  Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained from Adobe.
+ */
+
 const isBrowser = typeof window !== "undefined";
 
 export const onClientEntry = () => {
@@ -6,7 +13,6 @@ export const onClientEntry = () => {
     window._satellite = window._satellite || {};
     window.alloy_all = window.alloy_all || {};
     window.alloy_all.data = window.alloy_all.data || {};
-    window.alloy_all.data._adobe_corpnew = window.alloy_all.data._adobe_corpnew || {};
     window.alloy_all.data._adobe_corpnew = window.alloy_all.data._adobe_corpnew || {};
     window.alloy_all.data._adobe_corpnew.web = window.alloy_all.data._adobe_corpnew.web || {};
     window.alloy_all.data._adobe_corpnew.web.webPageDetails = window.alloy_all.data._adobe_corpnew.web.webPageDetails || {};
@@ -41,4 +47,4 @@ export const onRouteUpdate = ({ location, prevLocation }) => {
     // watch if analytics is online then track page
     const intervalId = setInterval(watchAndFireAnalytics, 1000);
   }
-} 
+}
